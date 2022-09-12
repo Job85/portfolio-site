74727c0e86ceab9de6a3cce444e982441f4c79ca
Job85 <julianjernigan@ymail.com>
2022-09-12 12:47:14 -0400

    created Home Page and imported it to App.js & set the Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-
+        <Route path='/' element={<Home />} />
       </Routes>
     </div>
   );
