2ae05dc2a98e13c0c30e44fbde296370a777af0c
Job85 <julianjernigan@ymail.com>
2022-09-12 19:43:50 -0400

    created header component with NavBar & Socials componentsimported

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Home from './pages/home';
 import About from './pages/about';
 import Projects from './pages/projects';
 import NavBar from './components/NavBar';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar /> */}
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar';
+import Socials from './Socials';
+
+const Header = () => {
+    return (
+        <div>
+            <NavBar />
+            <Socials />
+        </div>
+    )
+}
+
+export default Header
