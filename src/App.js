d724be13aaf7fa7f8d5ed4a59ddbe9adc5bb690d
Job85 <julianjernigan@ymail.com>
2022-09-12 12:49:47 -0400

    created About Page and imported it to App.js & set the Route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
+import About from './pages/about';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </div>
   );
