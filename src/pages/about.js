48ac09e1d324fb4e7f057eaddaf66cd5f1943387
Job85 <julianjernigan@ymail.com>
2022-09-12 19:47:47 -0400

    added place holders for h1, h2, & h3

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,16 @@ const About = () => {
     return (
         <div>
             This is My About Me Page
+            <div>
+                <h1>Who I am</h1>
+                <p>
+                    A bunch of text describing who I am
+                </p>
+                <h2>What I do</h2>
+                <p>List of Hard Skills</p>
+                <h3>How to reach me</h3>
+                <p>address@email.com</p>
+            </div>
         </div>
     )
 }
