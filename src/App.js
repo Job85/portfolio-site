8ccad3906308a9d28ba32be1dc68be104d7839b6
Job85 <julianjernigan@ymail.com>
2022-09-12 13:07:36 -0400

    created NavBar component and imported into App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import Home from './pages/home';
 import About from './pages/about';
 import Projects from './pages/projects';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
