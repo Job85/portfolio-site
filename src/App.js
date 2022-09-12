c72ce236b5ce69d8ebf0fbc43d47414da54e2178
Job85 <julianjernigan@ymail.com>
2022-09-12 12:52:29 -0400

    created Projects Page and imported it to App.js & set the Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import About from './pages/about';
+import Projects from './pages/projects';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
       </Routes>
     </div>
   );
---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Projects = () => {
+
+    return (
+        <div>
+            This is My Projects Page
+        </div>
+    )
+}
+
+export default Projects
