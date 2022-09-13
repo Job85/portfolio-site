180b8d9cec562e88664f216490419ccbbfac8bac
Job85 <julianjernigan@ymail.com>
2022-09-12 20:43:07 -0400

    imported Logo component and added as element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './NavBar';
 import Socials from './Socials';
+import Logo from './Logo'
 
 const Header = () => {
     return (
         <div>
+            <Logo />
             <NavBar />
             <Socials />
         </div>
