e59b3695b79930cc2e9e183a22d8fa32b539f7e8
Job85 <julianjernigan@ymail.com>
2022-09-13 18:29:33 -0400

    imported Parisienne font and applied to logo

--- src/components/StyledComponents.jsx
+++ src/components/StyledComponents.jsx
@@ -22,37 +22,31 @@ export const SocialBox = styled.div`
 
 // exports to Logo.jsx
 export const LogoBox = styled.div`
-    width: 100vw;
+    text-align: center;
+    text-shadow: -1px -1px orangered;
+    width: 50vw;
+    color: #ff44008e;
     align-self: flex-start;
     border: solid blue;
-`
-export const JLogo = styled.h1`    
-    @import url('https://fonts.googleapis.com/css2?family=Parisienne&effect=neon&display=swap');
-
-    text-align: center;
-    font-weight: 400;
+    `
+export const JLogo = styled.h1`
+   @import url('https://fonts.googleapis.com/css2?family=Parisienne&display=swap');
+   
     font-family: 'Parisienne', cursive;
+    color: #ff44008e;
     text-shadow: -1px -1px orangered;
+    font-weight: 400;
     display: inline-block;
     margin: 0;
-    /* transform: scale(1, -1);
-    -moz-transform: scale(1, -1);
-    -webkit-transform: scale(1, -1);
-    -o-transform: scale(1, -1);
-    -ms-transform: scale(1, -1);
-    transform: scale(1, -1); */
-    color: #ff44008e;
-    /* border: solid black; */
-    
 `;
 
 export const JLogo2 = styled.h1`    
-    @import url('https://fonts.googleapis.com/css2?family=Parisienne&effect=neon&display=swap');
-
-    text-align: center;
-    font-weight: 400;
+    @import url('https://fonts.googleapis.com/css2?family=Parisienne&display=swap');
+    
     font-family: 'Parisienne', cursive;
+    color: #ff44008e; 
     text-shadow: -1px -1px orangered;
+    font-weight: 400;
     display: inline-block;
     margin: 0;
     transform: scale(-1, 1);
@@ -60,8 +54,5 @@ export const JLogo2 = styled.h1`
     -webkit-transform: scale(-1, 1);
     -o-transform: scale(-1, 1);
     -ms-transform: scale(-1, 1);
-    transform: scale(-1, 1);
-    color: #ff44008e;
-    /* border: solid black; */
-    
+    transform: scale(-1, 1);    
 `;
