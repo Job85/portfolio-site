2c5541410420ac9d83d789ff17c68f28aec353b1
Job85 <julianjernigan@ymail.com>
2022-09-22 21:59:18 -0400

    deleted app.css import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import Home from './pages/home';
 import About from './pages/about';
 import Projects from './pages/projects';
