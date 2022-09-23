050cc4b9414148510f35ff32ee163484854a1a02
Job85 <julianjernigan@ymail.com>
2022-09-22 23:14:55 -0400

    moved components into Header folder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
 import Projects from './pages/projects';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 
 function App() {
   return (
