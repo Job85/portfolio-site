e8e370806b1ae77d1a422266ebd51074e15f7348
Job85 <julianjernigan@ymail.com>
2022-09-24 00:08:59 -0400

    added display: flex; direction row space-evenly

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import NavBar from './NavBar';
 import Socials from './Socials';
 // import Logo from './Logo'
+import '../../scss/layout/_header.scss';
 
 const Header = () => {
     return (
         <div className='container-fluid'>
-            {/* <Logo /> */}
-            <NavBar />
-            <Socials />
+            <div className='header'>
+                {/* <Logo /> */}
+                <NavBar />
+                <Socials />
+            </div>
         </div>
     )
 }
