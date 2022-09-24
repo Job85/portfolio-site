dc3889cb4735f75e47b46d418aa116786364fb2e
Job85 <julianjernigan@ymail.com>
2022-09-23 23:47:16 -0400

    imported main.scss

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './scss/main.scss'
 import './index.css';
 import App from './App';
 
