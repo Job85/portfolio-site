c95d682a28d2f0fa88637e68f44b3dce94fcc290
Job85 <julianjernigan@ymail.com>
2022-09-28 23:11:51 -0400

    restyled hero div: changed h1 font-family & color, changed h2 color, changed gradient color

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,20 +3,35 @@ import React from "react";
 const Home = () => {
 
     return (
-        <div className="hero">
-            <div>
-                <h1>
-                    Resolving complex problems <br />
-                    through innovative and efficient design
-                </h1>
+        <div className="container hero">
+            <div className="row">
+                <div className="col">
+                    <h1>
+                        Resolving Complex Problems <br />
+                        Through <span className="bold">Innovative</span> and <span className="bold">Efficient</span> Design
+                    </h1>
+                </div>
             </div>
-            <div>
-                <h2>
-                    Specializing as a full-stack software engineer, <br />
-                    Bringing a passion for creativity and collaborative skills from the hospitality industry <br />
-                    to the tech world.
-                </h2>
+            <div className="row">
+                <div className="col">
+                    <h2>
+                        Specializing as a full-stack software engineer, <br />
+                        Bringing a passion for creativity and collaborative skills from the hospitality industry <br />
+                        to the tech world.
+                    </h2>
+                </div>
             </div>
+            {/* <div className="row sign">
+                <div className="neon-sign col">
+                    <a
+                        className="btn neon"
+                        href="#"
+                        role='button'
+                    >
+                        Contact Me
+                    </a>
+                </div>
+            </div> */}
         </div>
     )
 }
