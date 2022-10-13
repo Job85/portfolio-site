fd7a6578cfdd950d2abef16693c03733fe9e4033
Job85 <julianjernigan@ymail.com>
2022-10-13 19:00:11 -0400

    created a list of hard skills

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,17 +4,56 @@ const About = () => {
 
     return (
         <div>
-            This is My About Me Page
-            <div>
-                <h1>Who I am</h1>
-                <p>
-                    A bunch of text describing who I am
-                </p>
-                <h2>What I do</h2>
-                <p>List of Hard Skills</p>
-                <h3>How to reach me</h3>
-                <p>address@email.com</p>
-            </div>
+            <h1>What I do</h1>
+            <h3>Languages</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Python</li>
+            </ul>
+            <h3>Frameworks</h3>
+            <ul>
+                <li>SCSS</li>
+                <li>Bootstrap</li>
+                <li>Express</li>
+                <li>Vue</li>
+                <li>Django</li>
+            </ul>
+            <h3>Libraries</h3>
+            <ul>
+                <li>React JS</li>
+                <li>Sequelize</li>
+                <li>Mongoose</li>
+
+            </ul>
+            <h3>Environments</h3>
+            <ul>
+                <li>Node JS</li>
+
+            </ul>
+            <h3>Databases</h3>
+            <h6>SQL</h6>
+            <ul>
+                <li>PostreSQL</li>
+            </ul>
+            <h6>NoSQL</h6>
+            <ul>
+                <li>
+                    <li>MongoDB</li>
+                </li>
+            </ul>
+            <h3>Misc.</h3>
+            <ul>
+                <li>RESTful API's</li>
+                <li>Full-stack application development</li>
+                <li>Full C.R.U.D Operations</li>
+                <li>Email JS</li>
+                <li>Json Web Token</li>
+                <li>Authentication</li>
+            </ul>
+            <h3>How to reach me</h3>
+            <a>JulianJrngn@gmail.com</a>
         </div>
     )
 }
