1b413846abe1ae81524781b90048547177758b02
Job85 <julianjernigan@ymail.com>
2022-10-14 21:34:36 -0400

    imported Hero component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,16 @@
 import React from "react";
 import NavBar from '../components/NavBar'
+import Hero from "../components/Hero";
 
 const Home = () => {
 
     return (
-        <div id="home" className="container hero">
+        <div id="home" className="container position-relative">
             <NavBar />
+            <Hero />
             <div className="row">
                 <div className="col">
-                    <h1>
+                    <h1 className="heading">
                         Resolving Complex Problems <br />
                         Through <span className="bold">Innovative</span> and <span className="bold">Efficient</span> Design
                         {/* <span className="sphere container"></ span> */}
@@ -17,7 +19,7 @@ const Home = () => {
             </div>
             <div className="row">
                 <div className="col">
-                    <h2>
+                    <h2 className="sub-heading">
                         Specializing as a full-stack software engineer, <br />
                         Bringing a passion for creativity and collaborative skills from the hospitality industry <br />
                         to the tech world.
