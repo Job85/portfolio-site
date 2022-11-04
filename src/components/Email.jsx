3a6561ee88bb4f709f2dac40ec81286b50425341
Job85 <julianjernigan@ymail.com>
2022-11-04 19:54:33 -0400

    added bootstrap button

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -5,7 +5,6 @@ const Email = () => {
         <a href="mailto:julianjrngn@gmail.com?subject=Great Portfolio!&body=Hello Julian, %0D%0A %0D%0A We'd like to set up an interview with you."
             role="button"
             className="btn btn-primary btn-lg"
-
         >
             Get In Touch!
         </a>
