492c972ec35f2e2cb9f0f7a06885262d9e9fe7b0
Job85 <joulescodinglab@gmail.com>
2023-03-13 23:40:34 -0400

    imported Card component

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Card from "../components/Card";
 
 const Projects = () => {
 
     return (
         <div id="projects">
             This is My Projects Page
+            <Card />
         </div>
     )
 }
