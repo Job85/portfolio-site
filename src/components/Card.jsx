d84d79ab960883c7a3c1c2c690de73cd4a1a22ec
Job85 <joulescodinglab@gmail.com>
2023-03-17 15:31:32 -0400

    styled bootstrap Card element to fit content

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,73 @@
 import React, { useState } from "react";
+import cn from "classnames";
+import { Card } from "react-bootstrap";
 
-const Card = ({ card }) => {
+const PCard = ({ card }) => {
 
     const [flip, setFlip] = useState(false);
 
+    const handleClick = () => {
+        if (card.variant === 'click') {
+            setFlip(!flip);
+        }
+    }
+
+    const handleFocus = () => {
+        if (card.variant === 'focus') {
+            setFlip(true);
+        }
+    }
+
+    const handleBlur = () => {
+        if (card.variant === 'focus') {
+            setFlip(false);
+        }
+    }
+
     return (
-        <div className="card-box">
-            <div className="card-container">
-                <div className="flip-card">
-                    <div className={flip ? 'flip' : ''}>
-                        <div className="front" onClick={() => setFlip(true)}>
+        <div className="container card-box">
+            <Card
+                tabIndex={card.id}
+                className={cn("flip-card h-200 w-450", {
+                    "focuse-trigger": card.variant === 'focus'
+                })}
+
+                onClick={handleClick}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+            >
+                <Card.Title
+                    className="flip-title"
+                >
+                    {card.title}
+                </Card.Title>
+                <div className="flip-card-outer">
+                    <div className={cn("flip-card-inner", {
+                        flip,
+                        'hover-trigger': card.variant === 'hover'
+                    })}>
+                        <div
+                            className="p-card front"
+                            variant="click"
+                        >
                             {card.front}
                         </div>
-                        <div className="back" onClick={() => setFlip(false)}>
+                        <div className="p-card back">
                             {card.back}
+                            <h3>Technologies</h3>
+                            {card.details}
+                            <div className="d-flex ps-4">
+                                <h5 className="pe-3">Website</h5>
+                                <Card.Link>
+                                    {card.link}
+                                </Card.Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Card>
         </div>
     )
 }
 
-export default Card
+export default PCard
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,72 +1,75 @@
 import React from "react";
-import Card from "../components/Card";
+import PCard from "../components/Card";
 import token from "../assets/Token_Huntr.png";
 import inventory from "../assets/Restaurant-Inventory-App.png";
+import qc from "../assets/QCDetailing.png"
 
 const cards = [
     {
-        id: '1',
-        front:
-            <img
-                src={token}
-                alt='Token Huntr App'
-                className="img-top"
-                style={{ width: 30 + 'rem' }}
-            />,
-        back:
-            <div>
-                <h3>
-                    Details
-                </h3>
-                <p>
-                    A geocaching application
-                </p>
-                <h3>
-                    Technologies
-                </h3>
-                <p>
-                    <ul>
-                        Languages - CSS, JavaScript, Axios, ReactJS, React Final-Form, Styled Components,
-                    </ul>
-                </p>
-                <h4>Website</h4>
-                <a
-                    href="https://token-huntr-app.web.app/"
-                >
-                    @Token Huntr
-                </a>
-            </div>
+        id: "1",
+        variant: "click",
+        front: <img
+            src={token}
+            alt='Token Huntr App'
+            className="img"
+            style={{ width: "inherit" }}
+        />,
+        title: "Token Huntr",
+        link:
+            <a
+                href="https://token-huntr-app.web.app/"
+                target="blank"
+            >
+                @Token Huntr
+            </a>,
+        details:
+            <p>
+                <h6>Languages</h6>
+                <ul> CSS, JavaScript</ul>
+                <h6>Libraries</h6>
+                <ul>
+                    React.js
+                    Styled-components
+                    validator.js
+                    Axios
+                    Font Awesome
+                </ul>
+                <h6>Database</h6>
+                <ul>PostgreSQL</ul>
+            </p>
+        ,
     },
     {
         id: '2',
+        variant: "click",
         front:
             <img
                 src={inventory}
                 alt='Inventory App'
                 className="img-top"
-                style={{ width: 30 + 'rem' }}
+                style={{ width: "inherit" }}
             />,
-        back:
-            <div>
-                <h3>
-                    Details
-                </h3>
-                <p>
-                    An application to streamline a restaurant's inventory process
-                </p>
-                <h3>
-                    Technologies
-                </h3>
-                <p>
-                    <ul>
-                        Languages - CSS, JavaScript, ReactJS, Axios,
-                    </ul>
-                </p>
-                <h4>Website</h4>
-                <a href="no_link">
-                    Undeployed
-                </a>
-            </div>
+        title: 'Inventory App',
+        back: "Back"
+    },
+    {
+        id: '3',
+        variant: 'click',
+        front:
+            <img
+                src={qc}
+                alt='Quality Cleaners'
+                className=""
+                style={{ width: "inherit" }}
+            />,
+        title: 'Quality Cleaners Detailing',
+        link:
+            <a
+                href="https://qcdetailing.net/"
+                target="blank"
+            >
+                QC Detailing
+            </a>
     }
 ];
 
@@ -74,10 +77,9 @@ const Projects = () => {
 
     return (
         <div id="projects">
-            This is My Projects Page
-            <div className="card-box">
+            <div className="card-wrapper col d-flex flex-column flex-md-row justify-content-around align-items-center">
                 {cards.map((card) =>
-                    <Card key={card.id} card={card} />
+                    <PCard key={card.id} card={card} />
                 )}
             </div>
         </div>
