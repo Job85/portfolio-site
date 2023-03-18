b4753a12b4aab2aa3447b0944cd5d8a16dc42a12
Job85 <joulescodinglab@gmail.com>
2023-03-17 23:58:29 -0400

    deleted unused imported component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Home from '../pages/home';
-import NavBar from "./NavBar";
 import About from "../pages/about";
 import Projects from "../pages/projects";
 import '../bootstrap/scss/components/_hero.scss';
+
 const Hero = () => {
 
     return (
