b37441064b03b65fa842d17bd0e246c95e7cb799
Job85 <joulescodinglab@gmail.com>
2023-03-17 23:56:35 -0400

    deleted unused imports and components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { HashLink as Link } from 'react-router-hash-link';
 import './App.scss';
 import './fonts/Benguiat\ Bold.ttf';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
-import Home from './pages/home';
-import About from './pages/about';
-import Projects from './pages/projects';
 
 function App() {
   return (
     <div className=''>
       <NavBar />
       <Hero />
-      {/* <About /> */}
-      {/* <Projects /> */}
     </div>
   );
 }
