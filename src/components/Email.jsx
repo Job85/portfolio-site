761351741bb1f9d14020afbfccac00357ac0e3c9
Job85 <joulescodinglab@gmail.com>
2023-04-03 02:46:09 -0400

    implemented emailJS and added modal to button click

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,13 +1,75 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import emailjs from '@emailjs/browser';
 
 const Email = () => {
+    const [showForm, setShowForm] = useState(false);
+
+    const form = useRef();
+
+    const serviceID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+    const templateID = process.env.REACT_APP_TEMPLATE_ID;
+    const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+
+    const handleButtonClick = () => {
+        setShowForm(true);
+        console.log(showForm)
+    }
+
+    const handleFormClose = () => {
+        setShowForm(false);
+    }
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm(serviceID, templateID, form.current, publicKey)
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
-        <a href="mailto:julianjrngn@gmail.com?subject=Great Portfolio!&body=Hello Julian, %0D%0A %0D%0A We'd like to set up an interview with you."
-            role="button"
-            className="btn btn-primary btn-lg"
-        >
-            Get In Touch!
-        </a>
+        <>
+            <button
+                onClick={handleButtonClick}
+                type="button"
+                className="btn btn-primary btn-lg"
+            >
+                Get In Touch!
+            </button>
+            {showForm && (
+                <div className="modal" tabIndex={-1}>
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h2 className="modal-title">Contact Form</h2>
+                                <button
+                                    onClick={handleFormClose}
+                                    type="button"
+                                    className="btn-close"
+                                    aria-label="Close"></button>
+                            </div>
+                            <form
+                                ref={form}
+                                onSubmit={sendEmail}
+                                className="modal-form"
+                            >
+                                <label>Name</label>
+                                <input type="text" name="from_name" />
+                                <label>Message</label>
+                                <textarea name="message" />
+                                <label>Contact Email or Phone</label>
+                                <textarea type="text" name="contact_us" />
+                                <div className="modal-footer">
+                                    <button type="submit">Send</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
 
