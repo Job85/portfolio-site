aac1b24170c61cafde3b876f3a2fc99a6b8c979d
Job85 <joulescodinglab@gmail.com>
2023-05-13 22:18:37 -0400

    removed LogoCanvas component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.scss';
 import './fonts/Benguiat Bold.ttf';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
-import LogoCanvas from './components/LogoCanvas';
 
 function App() {
   return (
     <div className=''>
       <NavBar />
       <Hero />
-      <LogoCanvas />
     </div>
   );
 }
