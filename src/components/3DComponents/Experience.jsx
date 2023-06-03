7f8866f80614edb9a6dd0cfac4a51b3276c32807
Job85 <joulescodinglab@gmail.com>
2023-06-03 01:58:38 -0400

    updated sun movement with useFrame

--- src/components/3DComponents/Experience.jsx
+++ src/components/3DComponents/Experience.jsx
@@ -2,54 +2,42 @@ import { Environment, OrbitControls, Sky } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
 
-
-
 const Experience = () => {
-    const [sunPos, setSunPos] = useState([0, 0, 0])
+    const [sunPosition, setSunPosition] = useState([0, 0, 0]);
+    const timeFactor = 0.1; // Adjust this value to control the speed of the animation
 
-    useFrame(({ clock }) => {
-        const elapsedTime = clock.getElapsedTime();
-
-        if (elapsedTime < 5) {
-            setSunPos([0, 0, 1]);
-        } else if (elapsedTime < 10) {
-            const progress = elapsedTime - 1;
-            const z = 50 - progress * 5;
-            setSunPos([0, 1, z])
-        } else {
-            const progress = elapsedTime - 10;
-            const z = -progress * 5;
-
-            setSunPos([0, 1, z])
+    useFrame((state, delta) => {
+        // Update sunPosition based on the specified steps and time factor
+        if (sunPosition[1] < 1) {
+            // Increment y value by 0.01 * timeFactor until it reaches 1
+            setSunPosition([0, sunPosition[1] + 0.01 * timeFactor, 1]);
+        } else if (sunPosition[2] > 0) {
+            // Decrease z value by increments of 0.1 * timeFactor until it reaches 0
+            setSunPosition([0, 1, sunPosition[2] - 0.1 * timeFactor]);
+        } else if (sunPosition[1] > 0.9) {
+            // Decrease y value by increments of 0.1 * timeFactor until it reaches 0.9
+            setSunPosition([0, sunPosition[1] - 0.1 * timeFactor, -1]);
+        } else if (sunPosition[1] > 0.01) {
+            // Decrease y value by increments of 0.01 * timeFactor until it reaches 0.01
+            setSunPosition([0, sunPosition[1] - 0.01 * timeFactor, -1]);
+        } else if (sunPosition[1] > 0.001) {
+            // Decrease y value by increments of 0.001 * timeFactor until it reaches 0.001
+            setSunPosition([0, sunPosition[1] - 0.001 * timeFactor, -1]);
         }
-    })
+    });
 
-    // useFrame(({ clock }) => {
-    //     const elapsedTime = clock.getElapsedTime();
-
-    //     const cycleDuration = 30;
-    //     const progress = elapsedTime % cycleDuration;
-
-    //     if (progress < 5) {
-    //         const z = 1 - progress * 0.2;
-    //         setSunPos([0, 0, z]);
-    //     } else {
-    //         const z = (progress -5) * 0.2
-    //         setSunPos([0, 1, z]);
-    //     }
-    // });
     return (
         <>
             <OrbitControls />
             <Environment preset="dawn" />
             <Sky
-                distance={450000}
-                sunPosition={sunPos}
+                distance={500}
+                sunPosition={sunPosition}
                 inclination={1}
-                azimuth={0.25}
+                azimuth={1}
             />
         </>
-    )
-}
+    );
+};
 
-export default Experience
+export default Experience;
