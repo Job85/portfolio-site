26db02ae7432899b7438ef7178fdf381e962b686
Job85 <joulescodinglab@gmail.com>
2023-06-12 23:52:32 -0400

    used extrudeGeometry component to create a shape along the curve path

--- src/components/3DComponents/Experience2.jsx
+++ src/components/3DComponents/Experience2.jsx
@@ -1,9 +1,10 @@
 import { OrbitControls, CatmullRomLine, useScroll, PerspectiveCamera } from "@react-three/drei";
 import { useMemo } from "react";
+import { CatmullRomCurve3, Vector3, Shape } from "three";
+import * as THREE from "three";
 import Background from "./Background";
 import { Ship } from "./Ship";
 import Marquee from "../Marquee";
-import { CatmullRomCurve3, Vector3 } from "three";
 
 const PATH_NB_POINTS = 4200;
 
@@ -31,6 +32,27 @@ const Experience2 = () => {
         return curve.getPoints(PATH_NB_POINTS);
     }, [travelPath])
 
+    const curve = useMemo(() => {
+        return new CatmullRomCurve3(travelPath.map((point) => new Vector3(...point)));
+    }, [])
+
+    // const shapePoints = useMemo(() => {
+    //     const points = travelPoints.map((point) => new Vector3(point.x, point.y, 0));
+    //     return points;
+    // }, [travelPoints]);
+
+    // const travelPoints = useMemo(() => {
+    //     return curve.getPoints(PATH_NB_POINTS);
+    // }, [curve])
+
+    const shape = useMemo(() => {
+        const shape = new THREE.Shape();
+        shape.moveTo(0, -0.5);
+        shape.lineTo(0, -0.5);
+
+        return shape;
+    }, [curve]);
+
     return (
         <>
             <OrbitControls />
@@ -45,13 +67,32 @@ const Experience2 = () => {
                 fov={30}
                 makeDefault
             />
-            <CatmullRomLine
-                points={travelPoints}
-                curveType="catmullrom"
-                tension={0.5}
-                color={0x90ff00}
-                lineWidth={4}
-            />
+            <group
+                position-y={-2}
+            >
+                <CatmullRomLine
+                    points={travelPoints}
+                    curveType="catmullrom"
+                    tension={0.5}
+                    color={0x90ff00}
+                    lineWidth={4}
+                />
+                <extrudeGeometry
+                    args={[
+                        shape,
+                        {
+                            steps: PATH_NB_POINTS,
+                            bevelEnabled: false,
+                            extrudePath: curve,
+                        }
+                    ]}
+                />
+                <meshStandardMaterial
+                    color={0x00ffef}
+                    opacity={0.7}
+                    transparent
+                />
+            </group>
             <Marquee
                 position={[0, -0.1, -100]}
                 text={"Resolving Complex\nProblems\nThrough Innovative\nand Efficient Design"}
