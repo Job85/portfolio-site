13e290ffe64c21a47e84afa7c63ee8e1aafab2dc
Job85 <joulescodinglab@gmail.com>
2023-06-22 17:12:22 -0400

    added NavBar component

--- src/App2.js
+++ src/App2.js
@@ -3,12 +3,11 @@ import './App.scss'
 import './fonts/Benguiat Bold.ttf';
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
-import { Nav } from "react-bootstrap";
 
 function App2() {
     return (
         <>
-            <Nav />
+            <NavBar />
             <Hero />
         </>
     )
