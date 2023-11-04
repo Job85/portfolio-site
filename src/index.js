ee695096eac72efdb2f6fc53d11171b60973b2ed
Job85 <joulescodinglab@gmail.com>
2023-11-04 00:17:48 -0400

    changed App => App2 in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './bootstrap/scss/main.scss'
 import './index.scss';
-import App from './App';
-// import App2 from './App2';
+// import App from './App';
+import App2 from './App2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-      {/* <App2 /> */}
+      {/* <App /> */}
+      <App2 />
     </BrowserRouter>
   </React.StrictMode>
 );
